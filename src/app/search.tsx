c9486b18023cbdc95977d2efd0c256a65ec6dbe4
Roman <>
2024-05-31 12:26:06 +0300

    Barcode scanner

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,13 +1,14 @@
 
 import { gql, useLazyQuery } from '@apollo/client';
-import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import React, { useState } from 'react';
 import { ActivityIndicator, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 import FoodListItem from '../components/FoodListItem';
 
-
 const query = gql`
-  query Query($ingr: String) {
-    search(ingr: $ingr) {
+  query Query($ingr: String, $upc: String) {
+    search(ingr: $ingr, upc: $upc) {
       text
       hints {
         food {
@@ -25,10 +26,13 @@ const query = gql`
 
 export default function SearchScreen() {
   const [searchValue, setSearchValue] = useState('');
+  const [scannerEnabled, setScannerEnabled] = useState(false);
 
-  const [runSearch, { data, loading, error }] = useLazyQuery(query, {
-    variables: { ingr: 'Pizza' }
-  });
+  const [runSearch, { data, loading, error }] = useLazyQuery(query);
+  const [permission, requestPermission] = useCameraPermissions();
+
+  // Request only if permission is not granded, and we can ask again
+  requestPermission();
 
   const foodItems = data?.search?.hints ?? [];
 
@@ -45,17 +49,59 @@ export default function SearchScreen() {
     return <Text>Failed to search</Text>;
   }
 
+  if (scannerEnabled) {
+    return (
+      <View style={{
+      }}>
+        <CameraView style={{
+          width: '100%',
+          height: '100%'
+        }}
+          onBarcodeScanned={(data) => {
+            runSearch({
+              variables: { upc: data.data },
+            });
+            setScannerEnabled(false);
+          }} />
+        <Ionicons
+          onPress={() => setScannerEnabled(false)}
+          name="close"
+          size={30}
+          color="dimgray"
+          style={{
+            position: 'absolute',
+            right: 10,
+            top: 10,
+          }}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder='Search...'
-        style={styles.input}
-        value={searchValue}
-        onChangeText={setSearchValue}
-      />
-      {searchValue && <Button title='Search' onPress={preformSearch} />}
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+      }}>
+        <TextInput
+          placeholder='Search...'
+          style={styles.input}
+          value={searchValue}
+          onChangeText={setSearchValue}
+        />
+        <Ionicons
+          onPress={() => setScannerEnabled(true)}
+          name="barcode-outline"
+          size={32}
+          color="black"
+        />
+      </View>
 
+      {searchValue && <Button title='Search' onPress={preformSearch} />}
       {loading && <ActivityIndicator />}
+
       <FlatList
         data={foodItems}
         renderItem={({ item }) => <FoodListItem item={item} />}
@@ -78,5 +124,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#f2f2f2',
     gap: 10,
     padding: 20,
+    flex: 1,
   }
 });
